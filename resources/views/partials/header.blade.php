6a8e48a104cfbbf516686892be3891a01bfe7748
rami3d <masmrami@gmail.com>
2016-01-05 09:12:56 +0000

    theme integrated

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse" role="banner">
+    <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/"><img src="images/logo.png" alt="logo"></a>
+            </div>
+        
+        <div class="collapse navbar-collapse navbar-right">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="/">Home</a></li>
+                <li><a href="contact-us.html">Contact</a></li>                        
+            </ul>
+        </div>
+    </div><!--/.container-->
+</nav><!--/nav-->
